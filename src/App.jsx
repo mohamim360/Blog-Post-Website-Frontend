701d8a0da381ebfc341d16d9598ba8bb09442ced
mohamim360 <mohamim360@gmail.com>
2025-10-05 19:41:01 +0600

    fix: _redirects file for handling client-side routing

--- src/App.jsx
+++ src/App.jsx
@@ -7,38 +7,35 @@ import ProtectedRoute from "./auth/ProtectedRoute";
 import BlogListingPage from "./components/Home/BlogListingPage";
 import RecentBlogs from "./components/Home/RecentBlogs";
 
-
 function App() {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          } />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
 
-          <Route path="/content-manager" element={
-            <ProtectedRoute>
-              <ContentManagerDashboard />
-            </ProtectedRoute>
-          } />
+          <Route
+            path="/content-manager"
+            element={
+              <ProtectedRoute>
+                <ContentManagerDashboard />
+              </ProtectedRoute>
+            }
+          />
 
           {/* Public routes */}
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignUpPage />} />
 
-          <Route path="/blog-list" element={
-           
-              <BlogListingPage />
-            
-          } />
-          <Route path="/recent-blogs" element={
-           
-              <RecentBlogs />
-            
-          } />
+          <Route path="/blog-list" element={<BlogListingPage />} />
+          <Route path="/recent-blogs" element={<RecentBlogs />} />
         </Routes>
       </div>
     </Router>
